16ecbc6d7398fdf8e55716a196028956b55ec224
eskaykaushik <eskaykaushik14@gmail.com>
2025-04-22 20:34:25 +0530

    added searchBar

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('theme', isLight ? 'light' : 'dark');
   });
 });
+
+
+document.getElementById('searchInput').addEventListener('keyup', function () {
+  const query = this.value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    const title = card.querySelector('.card-title').textContent.toLowerCase();
+    const desc = card.querySelector('.card-text').textContent.toLowerCase();
+
+    if (title.includes(query) || desc.includes(query)) {
+      card.parentElement.style.display = 'block';
+    } else {
+      card.parentElement.style.display = 'none';
+    }
+  });
+});
